d1b250f305ee89c366de15e2329ab45001e3abe5
william cruvinel <williamcruvinel@hotmail.com>
2025-12-13 21:51:44 -0300

    add scroll suave

--- script.js
+++ script.js
@@ -49,3 +49,21 @@ document.querySelectorAll('.nav-link').forEach((n) =>
     navMenu.classList.remove('active');
   }),
 );
+
+// SCROLL SUAVE
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    const targetId = this.getAttribute('href');
+    if (targetId === '#') return;
+
+    const targetElement = document.querySelector(targetId);
+    if (targetElement) {
+      window.scrollTo({
+        top: targetElement.offsetTop - 80,
+        behavior: 'smooth',
+      });
+    }
+  });
+});
