32ca6ebcd88c2dbc1d8c4e9400d4781345637005
william cruvinel <williamcruvinel@hotmail.com>
2025-12-13 21:51:09 -0300

    criando funcao toggle menu

--- script.js
+++ script.js
@@ -33,3 +33,19 @@ function updateThemeIcon() {
     themeIcon.className = 'fas fa-moon';
   }
 }
+
+// TOGGLE MENU
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.nav-menu');
+
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  navMenu.classList.toggle('active');
+});
+
+document.querySelectorAll('.nav-link').forEach((n) =>
+  n.addEventListener('click', () => {
+    hamburger.classList.remove('active');
+    navMenu.classList.remove('active');
+  }),
+);
